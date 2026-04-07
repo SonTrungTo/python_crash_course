20fb556171bab5d1d6bc09ac447bb5fb61ba134b
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-04-07 03:15:28 +0300

    add 4_5.py

---
+++ python_work/exercises/4_5.py
@@ -0,0 +1,6 @@
+LIMIT = 10 ** 6 # 1 million
+
+list_required = [value for value in range(1, LIMIT + 1)]
+print(f"Min={min(list_required)}")
+print(f"Max={max(list_required)}")
+print(f"Sum={sum(list_required)}")
