2d212327e05bd066c75893724fd44c256d229ac2
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-05-18 19:32:48 +0300

    fix: add slice.py

---
+++ python_work/slice.py
@@ -0,0 +1,5 @@
+# Include the third value in the brackets indicating a slice.
+players = ["charles", "martina", "michael", "florence", "eli"]
+print(f"full list: {players[:]}\n")
+print(f"skipped-2 partial list: {players[::2]}\n")
+print(f"skipped-3 partial list: {players[::3]}\n")
