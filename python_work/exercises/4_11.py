b146b994cbbcecdbe3a0d8980ee2848a232510cf
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-07 23:56:58 +0300

    fix: solve 4_11.py

--- python_work/exercises/4_11.py
+++ python_work/exercises/4_11.py
@@ -1,1 +1,14 @@
 ### 4.11 My Pizzas, Your Pizzas
+pizzas = ["pepperoni", "mushroom", "pineapple"]
+friend_pizzas = pizzas[:]
+
+pizzas.append("cheese")
+friend_pizzas.append("veggie")
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
