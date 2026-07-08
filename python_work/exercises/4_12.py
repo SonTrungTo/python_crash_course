1885dd4f7c63e10f1a51321785a61687410dd78e
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-08 05:10:43 +0300

    fix: finish 4_12.py

--- python_work/exercises/4_12.py
+++ python_work/exercises/4_12.py
@@ -1,1 +1,10 @@
 ### 4.12: More Loops
+foods = ["ramen", "instant_noodles", "bbh"]
+
+print(f"My favorite foods are: {", ".join([food for food in foods])}")
+print(f"")
+all_foods_str = ""
+for food in foods:
+    all_foods_str += f"{food}, "
+print(f"My favorite foods are: {all_foods_str[:-2]}")
+print(f"")
