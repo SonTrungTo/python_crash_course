a4320ee61bd67e349119748ccdd3cbffa1269520
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-16 02:05:18 +0300

    add 7_8.py

--- python_work/exercises/7_8.py
+++ python_work/exercises/7_8.py
@@ -1,1 +1,21 @@
 # Deli
+sandwich_orders: list[str] = [
+    "tuna",
+    "pepperoni",
+    "turkey",
+    "veggie",
+    "pastrami",
+    "salmon",
+    "turkey",
+    "cold_cuts"
+]
+finished_sandwiches: list[str] = []
+
+while sandwich_orders:
+    current_sandwich: str = sandwich_orders.pop(0)
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches have been made:")
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich}")
