82a462544a38c8783ca3c59eeafc3f5c2dde3da9
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-18 13:18:54 +0300

    add 8_1 and 8_2

---
+++ python_work/exercises/8_1.py
@@ -0,0 +1,6 @@
+# Message
+def display_message():
+    """Display a simple message."""
+    print("I am learning about functions in Python.")
+
+display_message()
---
+++ python_work/exercises/8_2.py
@@ -0,0 +1,6 @@
+# Favourite book
+def favourite_book(title):
+    """Display a message about the favourite book."""
+    print(f"One of my favourite books is {title.title()}.")
+
+favourite_book("microeconomic theory - mas colell, whinston, and green")
