870cfabc2122c6f3ef225ff2133792b1d7c1cdab
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-18 15:10:46 +0300

    fix: fix 8_7 format to conform with PEP8

--- python_work/exercises/8_7.py
+++ python_work/exercises/8_7.py
@@ -1,10 +1,15 @@
 # Album
-def make_album(artist: str, title: str, tracks: int = None) -> dict:
+from typing import Optional
+
+
+def make_album(artist: str, title: str, tracks: Optional[int] = None) -> dict:
     """Return a dictionary representing an album."""
     album = {'artist': artist.title(), 'title': title.title()}
     if tracks:
         album['tracks'] = tracks
     return album
+
+
 print(make_album('Metallica', 'Master of Puppets'))
 print(make_album('Geoff Zanelli', 'Squadron 42'))
 print(make_album('ABBA', 'The Best of ABBA, Vol. 1', tracks=12))
