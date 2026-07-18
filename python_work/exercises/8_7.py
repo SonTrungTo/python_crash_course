41ae4f2106cebcf30639436027b340a5f2e843b0
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-18 15:21:11 +0300

    fix: add 8_7 and 8_8

--- python_work/exercises/8_7.py
+++ python_work/exercises/8_7.py
@@ -9,7 +9,8 @@ def make_album(artist: str, title: str, tracks: Optional[int] = None) -> dict:
         album['tracks'] = tracks
     return album
 
-
-print(make_album('Metallica', 'Master of Puppets'))
-print(make_album('Geoff Zanelli', 'Squadron 42'))
-print(make_album('ABBA', 'The Best of ABBA, Vol. 1', tracks=12))
+# Guard it with if __name__ == '__main__': to prevent it from running when imported
+if __name__ == '__main__':
+    print(make_album('Metallica', 'Master of Puppets'))
+    print(make_album('Geoff Zanelli', 'Squadron 42'))
+    print(make_album('ABBA', 'The Best of ABBA, Vol. 1', tracks=12))
--- python_work/exercises/8_8.py
+++ python_work/exercises/8_8.py
@@ -1,1 +1,7 @@
 # User albums
+import importlib
+
+make_album = importlib.import_module('8_7').make_album
+
+print(make_album('Metallica', 'Master of Puppets'))
+print(make_album('Geoff Zanelli', 'Squadron 42'))
