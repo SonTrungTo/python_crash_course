ca546a2db8dd4869ebf09fa4f4563deeabdddc39
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-18 15:27:41 +0300

    fix: add 8_8

--- python_work/exercises/8_8.py
+++ python_work/exercises/8_8.py
@@ -3,5 +3,16 @@ import importlib
 
 make_album = importlib.import_module('8_7').make_album
 
-print(make_album('Metallica', 'Master of Puppets'))
-print(make_album('Geoff Zanelli', 'Squadron 42'))
+while True:
+    print("\nEnter 'q' at any time to quit.")
+    artist = input("Enter the artist's name: ")
+    title = input("Enter the album title: ")
+    tracks_input = input("Enter the number of tracks (or leave blank): ")
+    if artist.lower() == 'q' \
+    or title.lower() == 'q' \
+    or tracks_input.lower() == 'q':
+        break
+
+    tracks = int(tracks_input) if tracks_input else None
+    album = make_album(artist, title, tracks)
+    print(album)
