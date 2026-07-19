fc938e729fc17f9b983a43005bc1b8197bc871e7
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-19 19:51:21 +0300

    fix: add 8_10 and 8_11.py

--- python_work/exercises/8_10.py
+++ python_work/exercises/8_10.py
@@ -1,1 +1,19 @@
 # Sending Messages
+tms = [
+    "Hello, how are you?",
+    "I'm doing well, thank you!",
+    "What about you?",
+    "I'm good too!",
+    ]
+sent_messages = []
+
+def send_messages(messages: list[str], output_messages: list[str]) -> None:
+    """Prints each message in the list."""
+    while messages:
+        current_message = messages.pop(0)
+        print(f"Sending '{current_message}'")
+        output_messages.append(current_message)
+
+send_messages(tms, sent_messages)
+print(f"\nOriginal list of messages: {tms}")
+print(f"Sent messages: {sent_messages}")
