7cebfe666b9a4d0a9f99d57bec31ef61e7c6ef60
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-07-20 03:55:01 +0300

    fix: add 8_13.py

--- python_work/exercises/8_13.py
+++ python_work/exercises/8_13.py
@@ -1,1 +1,19 @@
 # User Profile
+def build_profile(first: str, last: str, **user_info: str) -> dict[str, str]:
+    """Build a dictionary containing everything we know about a user."""
+    profile = {}
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    for key in sorted(user_info.keys()):
+        profile[key] = user_info[key]
+    return profile
+
+
+user_profile = build_profile(
+    'son',
+    'to',
+    character='resilient',
+    personality='radical',
+    hobby='gaming'
+    )
+print(user_profile)
