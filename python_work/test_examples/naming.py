885f8559d20008a08f015af9ff7a6f8d4e133747
SonTrungTo <20899059+SonTrungTo@users.noreply.github.com>
2026-08-23 17:38:09 +0300

    fix: add some example tests

--- python_work/test_examples/naming.py
+++ python_work/test_examples/naming.py
@@ -1,4 +1,11 @@
-def get_formatted_name(first_name: str, last_name: str) -> str:
+def get_formatted_name(
+        first_name: str,
+        last_name: str,
+        middle_name: str = ""
+    ) -> str:
     """Format the name to have the first letter of each word capitalized."""
-    name = f"{first_name} {last_name}"
+    if middle_name:
+        name = f"{first_name} {middle_name} {last_name}"
+    else:
+        name = f"{first_name} {last_name}"
     return name.title()
--- python_work/test_examples/test_naming.py
+++ python_work/test_examples/test_naming.py
@@ -4,3 +4,8 @@ def test_first_last_name():
     """Test if "John Doe" is formatted correctly."""
     formatted_name = get_formatted_name("john", "doe")
     assert formatted_name == "John Doe"
+
+def test_first_middle_last_name():
+    """Test if "John Michael Doe" is formatted correctly."""
+    formatted_name = get_formatted_name("john", "doe", "michael")
+    assert formatted_name == "John Michael Doe"
